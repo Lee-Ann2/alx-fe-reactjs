f092ef00cebc99ae25bbad94b95399cc18baab8d
Lee-Ann2 <mfenyanakhazimla17@gmail.com>
2024-08-05 18:28:09 +0200

    Made some changes

--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -1,8 +1,8 @@
 function MainContent() {
     return (
-        <header>
-        <h1>My Favorite Cities</h1>
-        </header>
+        <main>
+        <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
     );
 }
 
