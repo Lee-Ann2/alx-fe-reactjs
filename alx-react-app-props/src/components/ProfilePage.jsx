34a6737725025e2160498ce38a0a16eaf64cb5d3
Lee-Ann2 <mfenyanakhazimla17@gmail.com>
2024-08-16 04:55:43 +0200

    lot of changes were made

--- alx-react-app-props/src/components/ProfilePage.jsx
+++ alx-react-app-props/src/components/ProfilePage.jsx
@@ -1,9 +1,6 @@
-import { useContext } from "react";
-import UserContext from './components/UserContext';
 import UserInfo from './UserInfo';
 
 function ProfilePage({userData}) {
-    const { name, age, bio } = useContext(UserContext);
     return (
         <UserInfo userData={userData} />
     );
--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,5 +1,8 @@
+import { useContext } from "react";
+import UserContext from './components/UserContext';
 const UserProfile = (props) => {
     return (
+      const { name, age, bio } = useContext(UserContext);
       <div>
         <h2>{props.name}</h2>
         <p>Age: {props.age}</p>
