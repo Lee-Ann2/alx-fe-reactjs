f80999b7cf03d910c7b53cacb2d9d572d7295c9f
Lee-Ann2 <mfenyanakhazimla17@gmail.com>
2024-08-20 03:33:03 +0200

    Basics of zust

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,15 @@
+import AddRecipeForm from './components/AddRecipeForm'
+import RecipeList from './components/RecipeList';
+
+function App() {
+  return (
+    <>
+    <div>
+      <AddRecipeForm />
+      <RecipeList />
+    </div>
+    </>
+  );
+}
+
+export default App;
