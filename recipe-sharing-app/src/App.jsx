a7c031c04f93596b112f38a00e86b1249bfed953
Lee-Ann2 <mfenyanakhazimla17@gmail.com>
2024-08-23 21:09:48 +0200

    fixed one last time

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
+import FavoritesList from "./components/FavoritesList";
+import RecommendationList from "./components/RecommendationsList";
 
 
 function App() {
