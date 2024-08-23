30fe2a38c67317a653063ba2774882fe1e32d9af
Lee-Ann2 <mfenyanakhazimla17@gmail.com>
2024-08-23 03:54:03 +0200

    Added the Edit button

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -6,6 +6,13 @@ const useRecipeStore = create(set => ({
   setRecipes: (recipes) => set({ recipes })
 }));
 
+updateRecipe: (updatedRecipe) => set(state => ({
+  recipes: state.recipes.map(recipe =>
+    recipe.id === updatedRecipe.id ? { ...recipe, ...updatedRecipe } : recipe
+  )
+}));
+
+
 deleteRecipe: (id) => set(state => ({
   recipes: state.recipes.filter(recipe => recipe.id != id)
 }));
