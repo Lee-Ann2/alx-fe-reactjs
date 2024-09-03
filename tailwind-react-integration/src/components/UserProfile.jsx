cf55d4030d9c1aaaa67054cd456581c5cf082c69
Lee-Ann2 <mfenyanakhazimla17@gmail.com>
2024-09-03 02:28:20 +0200

    Added responsive styling in the userprofile.jsx file

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,9 +1,9 @@
 function UserProfile() {
     return (
-      <div className="bg-gray-100 p-8 max-w-sm-400px mx-auto my-20 rounded-lg shadow-lg">
-        <img src="https://via.placeholder.com/150" alt="User" className="rounded-full w-36 h-36 max-auto" />
-        <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-        <p className="text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
+      <div className="bg-gray-100 p-8 max-w-sm-400px mx-auto my-20 rounded-lg shadow-lg sm:p-4 max-w-xs md:p-8 max-w-sm lg:p-12 max-w-sm">
+        <img src="https://via.placeholder.com/150" alt="User" className="rounded-full w-36 h-36 max-auto sm:w-24 h-24 md:w-36 h-36 lg:w-36 h-36" />
+        <h1 className="text-xl text-blue-800 my-4 sm:text-lg md:text-xl lg:text-xl">John Doe</h1>
+        <p className="text-gray-600 sm:text-sm md:text-base lg:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
       </div>
     );
   }
