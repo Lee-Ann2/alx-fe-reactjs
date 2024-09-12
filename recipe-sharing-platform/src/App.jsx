18c3eaa552dbdfb0b12a77d7d228cb07fea3b82c
Lee-Ann2 <mfenyanakhazimla17@gmail.com>
2024-09-13 00:54:01 +0200

    Added a HomePage.jsx

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -4,6 +4,7 @@ import RecipeDetails from "./components/RecipeDetails";
 import RecipeList from "./components/RecipeList";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RecommendationsList from "./components/RecommendationsList";
+import HomePage from "./components/HomePage";
 function App() {
   return (
     <>
@@ -12,11 +13,12 @@ function App() {
           <Route
             path="/"
             element={
-              <>
+              <> 
+                <HomePage />
                 <RecipeList />
                 <AddRecipeForm />
                 <FavoritesList />
-                <RecommendationList />
+                <RecommendationsList />
               </>
             }
           />
